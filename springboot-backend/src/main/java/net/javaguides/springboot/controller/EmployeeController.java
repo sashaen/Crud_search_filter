41595625b891b343708d3b894170d41afa52139e
sashaen <tanvybhola@gmail.com>
2023-03-13 16:29:51 +0530

    added x-query request header

--- springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import net.javaguides.springboot.repository.EmployeeRepository;
 import net.javaguides.springboot.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,30 +21,33 @@ public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
     // build create employee rest api and request body converts json into java obj and search and filter functionality
-    @GetMapping(produces = "application/xml")
+    @GetMapping
     public List<Employee> list( @RequestParam(value = "firstName", required = false)String firstName,
                                 @RequestParam(value = "lastName", required = false)String lastName,
                                 @RequestParam(value = "emailId", required = false)String emailId,
                                 @RequestParam(value = "jobPosition", required = false)String jobPosition,
-                                @RequestParam(value = "search", required = false) String searchstring){
+                                @RequestParam(value = "search", required = false) String searchstring,
+                                @RequestHeader("x-query") String xQueryHeader){
         Specification<Employee> specification = EmployeeService.getService(firstName, lastName, emailId, jobPosition, searchstring);
+
         return employeeRepository.findAll(specification);
     }
-    @PostMapping(produces = "application/xml")
-    public Employee createEmployee( @RequestBody Employee employee){
-       return employeeRepository.save(employee);
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee,  @RequestHeader("x-query") String xQueryHeader){
+
+        return employeeRepository.save(employee);
     }
 
     //build get employee by rest api
-    @GetMapping(value = "{id}", produces = "application/xml")
-    public ResponseEntity<Employee> getEmployeeById( @PathVariable Long id){
+    @GetMapping("{id}")
+    public ResponseEntity<Employee> getEmployeeById( @PathVariable Long id, @RequestHeader("x-query") String xQueryHeader){
        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
        return ResponseEntity.ok(employee);
     }
 
     //build update employee rest api
-    @PutMapping(value = "{id}", produces = "application/xml")
-    public ResponseEntity<Employee> updateEmployee( @PathVariable Long id, @RequestBody Employee employeeDetails){
+    @PutMapping("{id}")
+    public ResponseEntity<Employee> updateEmployee( @PathVariable Long id, @RequestBody Employee employeeDetails, @RequestHeader("x-query") String xQueryHeader){
        Employee updateEmployee = employeeRepository.findById(id).orElseThrow( ()-> new ResourceNotFoundException("Employee not exist with id: " + id));
        updateEmployee.setFirstName(employeeDetails.getFirstName());
        updateEmployee.setLastName(employeeDetails.getLastName());
@@ -56,8 +60,8 @@ public class EmployeeController {
     }
 
     // build delete employee rest api
-    @DeleteMapping(value = "{id}", produces = "application/xml")
-    public ResponseEntity<HttpStatus> deleteEmployee( @PathVariable Long id){
+    @DeleteMapping("{id}")
+    public ResponseEntity<HttpStatus> deleteEmployee( @PathVariable Long id, @RequestHeader("x-query") String xQueryHeader){
        Employee employee = employeeRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Employee not found with id:" + id));
 
        employeeRepository.delete(employee);
